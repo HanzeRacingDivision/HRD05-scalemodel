d293f6225d43bbda3298fa6ab2c168a1bc996555
Mark Oosting <markeasting@gmail.com>
2018-10-29 21:54:17 +0100

    Fixed encoder code to calculate each wheel RPM

--- encoders/encoders.ino
+++ encoders/encoders.ino
@@ -1,57 +1,72 @@
 // pins for the encoder inputs
-#define ENC_FR 3
-#define ENC_FL 1
-#define ENC_RR 2
-#define ENC_RL 3
+#define ENC_FR 2
+#define ENC_FL 3
+#define ENC_RR 4
+#define ENC_RL 5
 
 float WHEEL_DIAMETER = 80; // mm
 float WHEEL_CIRCUMFERENCE = WHEEL_DIAMETER * PI;
-int ENC_RESOLUTION = 1; // Amount of magnets
-int ENC_TIMER = 100; // ms
+int ENC_RESOLUTION = 4; // Number of magnets
 
-// variables to store the number of encoder pulses
-volatile unsigned long FR_Count = 0;
-volatile unsigned long FL_Count = 0;
-volatile unsigned long RR_Count = 0;
-volatile unsigned long RL_Count = 0;
+float FR_RPM = 0;
+float FL_RPM = 0;
+float RR_RPM = 0;
+float RL_RPM = 0;
 
-float FR_Speed = 0;
-float FL_Speed = 0;
-float RR_Speed = 0;
-float RL_Speed = 0;
+unsigned long time;
+unsigned long FR_latest_time;
+unsigned long FL_latest_time;
+unsigned long RR_latest_time;
+unsigned long RL_latest_time;
 
 void setup() {
-  pinMode(ENC_FR, INPUT);
-  pinMode(ENC_FL, INPUT);
-  pinMode(ENC_RR, INPUT);
-  pinMode(ENC_RL, INPUT);
+  pinMode(ENC_FR, INPUT_PULLUP);
+  pinMode(ENC_FL, INPUT_PULLUP);
+  pinMode(ENC_RR, INPUT_PULLUP);
+  pinMode(ENC_RL, INPUT_PULLUP);
 
-  // initialize hardware interrupts
-  attachInterrupt(digitalPinToInterrupt(ENC_FR), encoderEvent, CHANGE);
+  // Initialize hardware interrupts
+  attachInterrupt(digitalPinToInterrupt(ENC_FR), interrupt_FR, RISING);
+  attachInterrupt(digitalPinToInterrupt(ENC_FL), interrupt_FL, RISING);
+  attachInterrupt(digitalPinToInterrupt(ENC_RR), interrupt_RR, RISING);
+  attachInterrupt(digitalPinToInterrupt(ENC_RL), interrupt_RL, RISING);
   
   Serial.begin(115200);
 }
 
 void loop() {
 
-  int FR_Count_Before = FR_Count;
+  // Global time
+  time = millis();
+
+  // float FR_Speed = FR_RPM * WHEEL_CIRCUMFERENCE;
+
+  Serial.print(FR_RPM); Serial.print(" FR \t");
+  Serial.print(FL_RPM); Serial.print(" FL \t");
+  Serial.print(RR_RPM); Serial.print(" RR \t");
+  Serial.print(RL_RPM); Serial.print(" RL \t");
+  Serial.println();
   
-  delay(ENC_TIMER);
-
-  int FR_Count_After = FR_Count;
-
-  int FR_diff = FR_Count_After - FR_Count_Before;
-
-  FR_Speed = FR_diff * (WHEEL_CIRCUMFERENCE / ENC_RESOLUTION) / ENC_TIMER;
-
-  Serial.println(FR_Speed);
+  delay(50);
   
 }
 
-// encoder event for the interrupt call
-void encoderEvent() {
-  if (digitalRead(ENC_FR) == LOW) {
-    FR_Count++;
-  }
+// Interrupt events
+void interrupt_FR() {
+  FR_RPM = getRPMandResetEncTimer(FR_latest_time);
 }
- 
+void interrupt_FL() {
+  FL_RPM = getRPMandResetEncTimer(FL_latest_time);
+}
+void interrupt_RR() {
+  RR_RPM = getRPMandResetEncTimer(RR_latest_time);
+}
+void interrupt_RL() {
+  RL_RPM = getRPMandResetEncTimer(RL_latest_time);
+}
+
+int getRPMandResetEncTimer(unsigned long & encoder_time) {
+  int rpm = 60000 / ((millis() - encoder_time) * ENC_RESOLUTION);
+  encoder_time = millis();
+  return rpm;
+}
