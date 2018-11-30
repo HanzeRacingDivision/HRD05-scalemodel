5d92ba5fb9aec32bc5fc02bdf6c652080e2ce014
Mark Oosting <markeasting@gmail.com>
2018-11-30 16:08:27 +0100

    Create can_to_pwm.ino

---
+++ can_to_pwm/can_to_pwm.ino
@@ -0,0 +1,105 @@
+/*
+      __  __                          ____             _                ____  _       _      _           
+     / / / /___ _____  ____  ___     / __ \____ ______(_)___  ____ _   / __ \(_)   __(_)____(_)___  ____ 
+    / /_/ / __ `/ __ \/_  / / _ \   / /_/ / __ `/ ___/ / __ \/ __ `/  / / / / / | / / / ___/ / __ \/ __ \
+   / __  / /_/ / / / / / /_/  __/  / _, _/ /_/ / /__/ / / / / /_/ /  / /_/ / /| |/ / (__  ) / /_/ / / / /
+  /_/ /_/\__,_/_/ /_/ /___/\___/  /_/ |_|\__,_/\___/_/_/ /_/\__, /  /_____/_/ |___/_/____/_/\____/_/ /_/ 
+  
+  CAN Reciever code for the HRD05
+  Mark Oosting, 2019
+  
+*/
+
+#include <CAN.h>
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  Serial.println("CAN Receiver");
+
+  // start the CAN bus at 500 kbps
+  if (!CAN.begin(500E3)) {
+    Serial.println("Starting CAN failed!");
+    while (1);
+  }
+}
+
+void loop() {
+  // try to parse packet
+  int packetSize = CAN.parsePacket();
+
+  byte data[8];
+  
+  if(packetSize) {
+  
+    int packetID = CAN.packetId();
+    
+    unsigned int i = 0;
+    
+    while (CAN.available() && (i < 8)) {
+      data[i] = CAN.read();
+      i++;
+    }
+
+    Serial.println(data[0]);
+
+//    for(int i = 0; i < sizeof(data); i++){
+//      Serial.print("Byte ");
+//      Serial.print(i);
+//      Serial.print(": ");
+//      Serial.print(data[i]);
+//      Serial.println();
+//    }
+    
+    switch (packetID) {
+      case 0x12:
+        while (CAN.available() && (i < 8)) {
+          data[i] = CAN.read();
+          i++;
+        }
+        break;
+      default:
+        Serial.print("Unknown ID: ");
+        Serial.print(packetID);
+        Serial.println();
+        break;
+    }
+  }
+  
+//  Serial.println(data[0]);
+//  Serial.println();
+
+//  if (packetSize) {
+//    // received a packet
+//    Serial.print("Received ");
+//
+//    if (CAN.packetExtended()) {
+//      Serial.print("extended ");
+//    }
+//
+//    if (CAN.packetRtr()) {
+//      // Remote transmission request, packet contains no data
+//      Serial.print("RTR ");
+//    }
+//
+//    Serial.print("packet with id 0x");
+//    Serial.print(CAN.packetId(), HEX);
+//
+//    if (CAN.packetRtr()) {
+//      Serial.print(" and requested length ");
+//      Serial.println(CAN.packetDlc());
+//    } else {
+//      Serial.print(" and length ");
+//      Serial.println(packetSize);
+//
+//      // only print packet data for non-RTR packets
+//      while (CAN.available()) {
+//        Serial.println((char)CAN.read());
+//      }
+//      Serial.println();
+//    }
+//
+//    Serial.println();
+//  }
+}
