3a7703e87b39b5d2140e62f5d780b498de008bd4
Mark Oosting <markeasting@gmail.com>
2018-12-01 21:26:06 +0100

    Updated all CAN to PWM logic + added failsafe for lost connection

--- can_to_pwm/can_to_pwm.ino
+++ can_to_pwm/can_to_pwm.ino
@@ -6,7 +6,7 @@
   /_/ /_/\__,_/_/ /_/ /___/\___/  /_/ |_|\__,_/\___/_/_/ /_/\__, /  /_____/_/ |___/_/____/_/\____/_/ /_/ 
   
   CAN Reciever code for the HRD05
-  Mark Oosting, 2019
+  Mark Oosting, Wout Veltman, 2019
   
 */
 
@@ -14,21 +14,25 @@
 
 #define ESC1_OUT 3
 #define ESC2_OUT 5
-#define SERVO_OUT 9
+#define STEER_OUT 9
+#define MAX_TIMEOUT 500
 
-int THROTTLE = 0;
-int REVERSE = 0;
-int STEER = 0;
+int MOTOR1 = 191;
+int MOTOR2 = 191;
+int STEER = 194; // Calibrate this value
 
-void setup() {  
+unsigned long current_time;
+unsigned long latest_message_time;
+
+void setup() {
   pinMode(ESC1_OUT, OUTPUT);
   pinMode(ESC2_OUT, OUTPUT);
-  pinMode(SERVO_OUT, OUTPUT);
+  pinMode(STEER_OUT, OUTPUT);
    
   Serial.begin(115200);
   while (!Serial);
 
-  Serial.println("CAN Receiver");
+  Serial.println("CAN RECEIVER");
 
   // start the CAN bus at 500 kbps
   if (!CAN.begin(500E3)) {
@@ -38,50 +42,50 @@ void setup() {
 }
 
 void loop() {
-  // try to parse packet
+
+  // RECEIVE CAN MESSAGES
   int packetSize = CAN.parsePacket();
 
-  byte data[8];
+  byte data[11];
+
+  unsigned int i = 0;
   
   if(packetSize) {
-  
-    int packetID = CAN.packetId();
     
-    unsigned int i = 0;
-    
-    while (CAN.available() && (i < 8)) {
+    while (CAN.available()) {
       data[i] = CAN.read();
       i++;
     }
-
-    int CANthrottle = data[0];
-  
-//    for(int i = 0; i < sizeof(data); i++){
-//      Serial.print("Byte ");
-//      Serial.print(i);
-//      Serial.print(": ");
-//      Serial.print(data[i]);
-//      Serial.println();
-//    }
+    
+    int packetID = CAN.packetId();
     
     switch (packetID) {
+      
+      // MOTOR CONTROL
       case 0x12:
-        while (CAN.available() && (i < 8)) {
-          data[i] = CAN.read();         
-          i++;
-        }
 
-        THROTTLE = map(data[0], 0, 255, 191, 255);
-        REVERSE = map(data[1], 0, 255, 191, 127);
-        STEER = map(data[2], 0, 255, 127, 255);
+        //REVERSE = map(data[1], 0, 255, 191, 127);
+        
+        MOTOR2 = map(data[0], 0, 255, 191, 220);
+        MOTOR1 = map(data[0], 0, 255, 191, 220); // Map this to the second motor value
 
-        
-        Serial.println(THROTTLE);
-        Serial.println(REVERSE);
-        Serial.println(STEER);
-        
+        latest_message_time = millis();
         
         break;
+
+
+        
+      // STEERING CONTROL; MID: 194, Right: 155, Left: 234
+      case 0x15:
+
+        STEER = map(data[0], 0, 255, 155, 234);
+        
+        latest_message_time = millis();
+        
+        break;
+        
+
+      // FALLBACK
       default:
         Serial.print("Unknown ID: ");
         Serial.print(packetID);
@@ -90,39 +94,30 @@ void loop() {
     }
   }
   
-//  Serial.println(data[0]);
-//  Serial.println();
+  current_time = millis();
+  
+  // Reset to safe values if CAN communication is lost
+  if(current_time - latest_message_time > MAX_TIMEOUT) {
+    
+    MOTOR1 = 191;
+    MOTOR2 = 191;
+    STEER = 194;
+    Serial.println("CAN TIMEOUT");
+    
+  } else {
+    Serial.print("STEER: ");
+    Serial.print(STEER);
+    Serial.print("\t");
+    Serial.print("MOTOR1: ");
+    Serial.print(MOTOR1);
+    Serial.print("\t");
+    Serial.print("MOTOR2: ");
+    Serial.print(MOTOR2);
+    Serial.println();
+  }
 
-//  if (packetSize) {
-//    // received a packet
-//    Serial.print("Received ");
-//
-//    if (CAN.packetExtended()) {
-//      Serial.print("extended ");
-//    }
-//
-//    if (CAN.packetRtr()) {
-//      // Remote transmission request, packet contains no data
-//      Serial.print("RTR ");
-//    }
-//
-//    Serial.print("packet with id 0x");
-//    Serial.print(CAN.packetId(), HEX);
-//
-//    if (CAN.packetRtr()) {
-//      Serial.print(" and requested length ");
-//      Serial.println(CAN.packetDlc());
-//    } else {
-//      Serial.print(" and length ");
-//      Serial.println(packetSize);
-//
-//      // only print packet data for non-RTR packets
-//      while (CAN.available()) {
-//        Serial.println((char)CAN.read());
-//      }
-//      Serial.println();
-//    }
-//
-//    Serial.println();
-//  }
+//  analogWrite(ESC1_OUT, MOTOR1);
+//  analogWrite(ESC2_OUT, MOTOR2);
+  analogWrite(STEER_OUT, STEER);
+
 }
